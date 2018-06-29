3b198e8599a228e18a8348a578007c9962019ca4
natallia.karanevich <natallia.karanevich@gmail.com>
2018-06-29 13:41:25 +0300

    [feature/AD-282-autocompleteInfinite] build

--- dist/react-select.js
+++ dist/react-select.js
@@ -2174,7 +2174,11 @@ var Async = function (_Component) {
 			// Ignore all but the most recent request
 			this._callback = callback;
 
-			var promise = loadOptions(inputValue, callback);
+			for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+				args[_key - 1] = arguments[_key];
+			}
+
+			var promise = loadOptions.apply(undefined, [inputValue, callback].concat(args));
 			if (promise) {
 				promise.then(function (data) {
 					return callback(null, data);
